f38374f83845c4bab13aaba3184ac4359a4169f4
Aryan <aaggarwal@3clogic.com>
2021-05-08 13:53:39 +0530

    Frontend

---
+++ src/components/Messages/Messages.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import ScrollToBottom from 'react-scroll-to-bottom';
+
+import Message from './Message/Message';
+
+import './Messages.css';
+
+const Messages = ({ messages, name }) => (
+    <div>
+  {/* <ScrollToBottom className="messages"> */}
+    {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
+  {/* </ScrollToBottom> */}
+  </div>
+);
+
+export default Messages;
